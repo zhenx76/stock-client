1122709b343d5eb506ced4e6f75f6fd61ed8feab
Zhen Xie <zhen@almadendigitallabs.com>
2016-09-16 21:44:09 -0700

    Enable browserSync proxy to allow cross domain API calls

--- gulp/server.js
+++ gulp/server.js
@@ -35,7 +35,7 @@ function browserSyncInit(baseDir, browser)
      *
      * For more details and option, https://github.com/chimurai/http-proxy-middleware/blob/v0.9.0/README.md
      */
-    // server.middleware = proxyMiddleware('/users', {target: 'http://jsonplaceholder.typicode.com', changeOrigin: true});
+    server.middleware = proxyMiddleware('/api/v1', {target: 'http://127.0.0.1:8080', changeOrigin: true});
 
     browserSync.instance = browserSync.init({
         startPath: '/',
